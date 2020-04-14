8755120de3080cbda7e8404af0f98fc248320bd7
madeleinewoodbury <randine25@gmail.com>
2020-04-14 16:09:54 -0500

    Cors access control

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ connectDB();
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Origin', [
+    'http://localhost:3000/',
+    'https://sharp-clarke-2b9a68.netlify.com/',
+  ]);
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
