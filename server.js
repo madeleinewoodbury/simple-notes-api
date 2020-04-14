abcda8edabee215f966eb544fc162be2a0b9c571
madeleinewoodbury <randine25@gmail.com>
2020-04-14 10:38:55 -0500

    Connect to database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
