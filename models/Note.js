0c125302a3d5430c232fc54d7d36ff3d899694df
madeleinewoodbury <randine25@gmail.com>
2020-04-14 10:41:48 -0500

    User and Note models

---
+++ models/Note.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Note = mongoose.model('note', NoteSchema);
