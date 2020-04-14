2382d58e2254243d8376031faab7f1024778074d
madeleinewoodbury <randine25@gmail.com>
2020-04-14 10:56:12 -0500

    Register User

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,89 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-// @route     GET api/users
-// @desc      Test Route
+// @route     GET api/users/
+// @desc      Get all users
 // @access    Public
-router.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Users Test Route' });
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    if (!users) {
+      return res.status(400).json({ msg: 'No users found' });
+    }
+
+    res.status(200).json({ count: users.length, data: users });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
+// @route     POST api/users/
+// @desc      Register new user
+// @access    Public
+router.post(
+  '/',
+  [
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user already exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // Create new user
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Encrypt the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Create the payload
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Respond with jsonwebtoken
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
